9fc0c2dce539d42931dfa8397fff72daf9aca6e6
creacion de proyecto <8432200068@univermilenium.edu.mx>
2025-05-17 14:30:54 -0600

    segundo comit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//--------- declaracion de tutas fijas------//
+Route::get('productos',function(){
+    return "Seccion de productos";
+});
+
+//-------declaracion de parametros--------//
+Route::get('productos/{id}',function ($id){
+    return  "Bienvenidos $id";
+});
+
+Route::get('prueba_parametros/{id}', function ($id) {
+    $d = $id;
+    $nombre = explode("-",$d);
+    return "tu nombre es $nombre[1]";
 });
